bcb01229ad7507d27c5c6a2a7f4d0ecfa8cb4250
ChrisGibbs74 <crgibbs@wisc.edu>
2026-05-12 17:24:11 +0200

    Wire all routes into server.js

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const express = require('express')
 const cors = require('cors')
 const mongoose = require('mongoose')
 
+const authRoutes = require('./routes/auth')
+const visitsRoutes = require('./routes/visits')
+const leaderboardRoutes = require('./routes/leaderboard')
+
 const app = express()
 
 app.use(cors())
@@ -11,6 +15,10 @@ app.use(express.json())
 app.get('/', (req, res) => {
   res.json({ message: 'WanderList server is running' })
 })
+
+app.use('/api/auth', authRoutes)
+app.use('/api/visits', visitsRoutes)
+app.use('/api/leaderboard', leaderboardRoutes)
 
 mongoose.connect(process.env.MONGODB_URI)
   .then(() => {
